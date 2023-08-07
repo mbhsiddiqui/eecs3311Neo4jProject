b546cb0ebc84f522a0316550cd8b23bf304acdd4
Maaz Siddiqui <msiddiqi@my.yorku.ca>
2023-08-06 23:19:42 -0400

    added multi-threading

--- src/main/java/ca/yorku/eecs/App.java
+++ src/main/java/ca/yorku/eecs/App.java
@@ -2,18 +2,25 @@ package ca.yorku.eecs;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 import com.sun.net.httpserver.HttpServer;
-import org.neo4j.driver.v1.*;
+import org.neo4j.driver.v1.AuthTokens;
+import org.neo4j.driver.v1.Config;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.GraphDatabase;
 
 public class App 
 {
     static int PORT = 8080;
     public static void main(String[] args) throws IOException
     {
-        Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "12345678"), Config.build().withoutEncryption().toConfig());
         HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", PORT), 0);
         // TODO: two lines of code are expected to be added here
         // please refer to the HTML server example
+        server.setExecutor(Executors.newCachedThreadPool());
+        Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "12345678"), Config.build().withoutEncryption().toConfig());
         server.createContext("/api/v1/addActor", new AddActorHandler(driver));
         server.start();
         System.out.printf("Server started on port %d...\n", PORT);
