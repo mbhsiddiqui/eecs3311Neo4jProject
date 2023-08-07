5d1c66071efe0881177ff43072f667f4688dc62a
Maaz Siddiqui <msiddiqi@my.yorku.ca>
2023-08-06 22:11:16 -0400

    implemented PUT request for /api/v1/addActor endpoint

---
+++ src/main/java/ca/yorku/eecs/AddActorHandler.java
@@ -0,0 +1,58 @@
+package ca.yorku.eecs;
+
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import org.neo4j.driver.v1.*;
+import java.io.IOException;
+import org.json.JSONObject;
+
+public class AddActorHandler implements HttpHandler {
+    Driver driver;
+
+    public AddActorHandler(Driver driver) {
+        this.driver = driver;
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        // Read the request body
+        String body = Utils.getBody(exchange);
+
+        try (Session session = driver.session()) {
+            // Parse the JSON body
+            JSONObject json = new JSONObject(body);
+            String name = json.getString("name");
+            String actorId = json.getString("actorId");
+
+            // Start a transaction
+            try (Transaction tx = session.beginTransaction()) {
+                // Check if the actorId already exists
+                StatementResult result = tx.run("MATCH (a:Actor {actorId: $actorId}) RETURN a", Values.parameters("actorId", actorId));
+
+                if (result.hasNext()) {
+                    // Actor with given actorId already exists
+                    String response = "Actor with given actorId already exists.";
+                    exchange.sendResponseHeaders(400, response.length());
+                    exchange.getResponseBody().write(response.getBytes());
+                } else {
+                    // Create new actor
+                    tx.run("CREATE (a:Actor {name: $name, actorId: $actorId})", Values.parameters("name", name, "actorId", actorId));
+
+                    String response = "Actor added successfully.";
+                    exchange.sendResponseHeaders(200, response.length());
+                    exchange.getResponseBody().write(response.getBytes());
+                }
+
+                // Commit the transaction
+                tx.success();
+            }
+        } catch (Exception e) {
+            // Exception occurred
+            String response = "Internal server error.";
+            exchange.sendResponseHeaders(500, response.length());
+            exchange.getResponseBody().write(response.getBytes());
+        } finally {
+            exchange.getResponseBody().close();
+        }
+    }
+}
--- src/main/java/ca/yorku/eecs/App.java
+++ src/main/java/ca/yorku/eecs/App.java
@@ -3,15 +3,18 @@ package ca.yorku.eecs;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import com.sun.net.httpserver.HttpServer;
+import org.neo4j.driver.v1.*;
 
 public class App 
 {
     static int PORT = 8080;
     public static void main(String[] args) throws IOException
     {
+        Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "12345678"), Config.build().withoutEncryption().toConfig());
         HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", PORT), 0);
         // TODO: two lines of code are expected to be added here
-        // please refer to the HTML server example 
+        // please refer to the HTML server example
+        server.createContext("/api/v1/addActor", new AddActorHandler(driver));
         server.start();
         System.out.printf("Server started on port %d...\n", PORT);
     }
