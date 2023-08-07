8b971fbce0a3c4af17dea54ffbdd89f122d3f48e
Maaz Siddiqui <msiddiqi@my.yorku.ca>
2023-08-06 23:33:54 -0400

    update App.java

--- src/main/java/ca/yorku/eecs/App.java
+++ src/main/java/ca/yorku/eecs/App.java
@@ -15,11 +15,9 @@ public class App
     public static void main(String[] args) throws IOException
     {
         HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", PORT), 0);
-        // TODO: two lines of code are expected to be added here
-        // please refer to the HTML server example
-        server.setExecutor(Executors.newCachedThreadPool());
         Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "12345678"), Config.build().withoutEncryption().toConfig());
         server.createContext("/api/v1/addActor", new AddActorHandler(driver));
+        server.setExecutor(Executors.newCachedThreadPool());
         server.start();
         System.out.printf("Server started on port %d...\n", PORT);
     }
