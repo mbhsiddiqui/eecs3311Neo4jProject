a679f77deeb174545b1298b9e5ce614e3a042f11
Tyluur <contact@tyluur.com>
2023-08-07 19:01:20 -0400

    feat: add AddRelationshipHandlerTest

---
+++ src/test/java/ca/yorku/eecs/api/put/AddRelationshipHandlerTest.java
@@ -0,0 +1,82 @@
+package ca.yorku.eecs.api.put;
+
+import ca.yorku.eecs.api.put.AddRelationshipHandler;
+import com.sun.net.httpserver.HttpExchange;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.neo4j.driver.v1.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AddRelationshipHandlerTest {
+
+	@Mock
+	private HttpExchange httpExchange;
+
+	@Mock
+	private Driver driver;
+
+	@Mock
+	private Session session;
+
+	@Mock
+	private Transaction transaction;
+
+	@Mock
+	private StatementResult actorResult;
+
+	@Mock
+	private StatementResult movieResult;
+
+	@Mock
+	private StatementResult relationResult;
+
+	@Mock
+	private OutputStream outputStream;
+
+	@Before
+	public void setUp() throws IOException {
+		when(httpExchange.getRequestBody()).thenReturn(new ByteArrayInputStream("{\"actorId\": \"123\", \"movieId\": \"456\"}".getBytes()));
+		when(httpExchange.getResponseBody()).thenReturn(outputStream);
+		when(driver.session()).thenReturn(session);
+		when(session.beginTransaction()).thenReturn(transaction);
+	}
+
+	@Test
+	public void testAddRelationshipHandlerSuccess() throws IOException {
+		when(transaction.run(anyString(), any(Value.class))).thenReturn(actorResult, movieResult, relationResult);
+		when(actorResult.hasNext()).thenReturn(true);
+		when(movieResult.hasNext()).thenReturn(true);
+		when(relationResult.hasNext()).thenReturn(false);
+
+		AddRelationshipHandler handler = new AddRelationshipHandler(driver);
+		handler.handle(httpExchange);
+
+		verify(outputStream).write(any(byte[].class));
+		verify(outputStream).close();
+		verify(transaction, times(1)).success();
+	}
+
+	@Test
+	public void testAddRelationshipHandlerFailureDueToExistingRelationship() throws IOException {
+		when(transaction.run(anyString(), any(Value.class))).thenReturn(actorResult, movieResult, relationResult);
+		when(actorResult.hasNext()).thenReturn(true);
+		when(movieResult.hasNext()).thenReturn(true);
+		when(relationResult.hasNext()).thenReturn(true);
+
+		AddRelationshipHandler handler = new AddRelationshipHandler(driver);
+		handler.handle(httpExchange);
+
+		verify(outputStream).write(any(byte[].class));
+		verify(outputStream).close();
+		verify(transaction, never()).success();
+	}
+}
