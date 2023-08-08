cfb09cd6506b9050640d36aaf5ebd1fcb6a963af
Tyluur <contact@tyluur.com>
2023-08-07 22:45:09 -0400

    feat: add index page handler with table

--- src/main/java/ca/yorku/eecs/handler/RootHandler.java
+++ src/main/java/ca/yorku/eecs/handler/RootHandler.java
@@ -9,48 +9,28 @@ import java.io.OutputStream;
 import java.util.List;
 import java.util.stream.Collectors;
 
-/**
- * Handles HTTP requests to the root path ("/").
- * <p>
- * This handler returns an HTML page with links to each of the API endpoints and brief descriptions of what they do.
- * It also lists the first 10 actors and movies from the Neo4j database with all their details.
- * </p>
- * <p>
- * Implements the HttpHandler interface to handle the HTTP request and response.
- * </p>
- *
- * @since 2023-08-07
- */
 public class RootHandler implements HttpHandler {
 
-	/**
-	 * The Neo4j database driver instance used for database operations.
-	 */
 	private final Driver driver;
 
-	/**
-	 * Constructs a new RootHandler with the provided Neo4j driver.
-	 *
-	 * @param driver The Neo4j driver instance.
-	 */
 	public RootHandler(Driver driver) {
 		this.driver = driver;
 	}
 
-	/**
-	 * Handles the HTTP request and constructs the response.
-	 * <p>
-	 * Retrieves the first 10 actors and movies from the database and includes them in an HTML page along with links to API endpoints.
-	 * </p>
-	 *
-	 * @param exchange The HTTP exchange object containing request and response details.
-	 * @throws IOException If an I/O error occurs.
-	 */
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
-		// Fetch first 10 actors and movies from the database
 		List<String> actors = fetchFirstTenActors();
 		List<String> movies = fetchFirstTenMovies();
+
+		StringBuilder actorTableContent = new StringBuilder();
+		for (String actor : actors) {
+			actorTableContent.append(actor);
+		}
+
+		StringBuilder movieTableContent = new StringBuilder();
+		for (String movie : movies) {
+			movieTableContent.append(movie);
+		}
 
 		String htmlResponse = "<html>" +
 				"<head>" +
@@ -75,14 +55,12 @@ public class RootHandler implements HttpHandler {
 				"<li><a href='/api/v1/computeBaconPath'>/api/v1/computeBaconPath</a>: Computes the Bacon path for a given actor.</li>" +
 				"</ul>" +
 				"<h2>First 10 Actors:</h2>" +
-				"<table>" +
-				"<tr><th>Name</th><th>ActorId</th><th>Movies Acted In</th></tr>" +
-				actors.stream().collect(Collectors.joining()) +
+				"<table><tr><th>Name</th><th>ActorId</th><th>Movies Acted In</th></tr>" +
+				actorTableContent.toString() +
 				"</table>" +
 				"<h2>First 10 Movies:</h2>" +
-				"<table>" +
-				"<tr><th>Name</th><th>MovieId</th><th>Actors</th></tr>" +
-				movies.stream().collect(Collectors.joining()) +
+				"<table><tr><th>Name</th><th>MovieId</th><th>Actors</th></tr>" +
+				movieTableContent.toString() +
 				"</table>" +
 				"</body>" +
 				"</html>";
@@ -93,35 +71,27 @@ public class RootHandler implements HttpHandler {
 		os.close();
 	}
 
-	/**
-	 * Fetches the first 10 actors from the Neo4j database.
-	 * <p>
-	 * For each actor, it retrieves the name, actorId, and the list of movies they have acted in.
-	 * </p>
-	 *
-	 * @return A list of HTML table rows with details about the actors.
-	 */
 	private List<String> fetchFirstTenActors() {
-		// Implement database query to fetch first 10 actors
 		try (Session session = driver.session()) {
-			StatementResult result = session.run("MATCH (a:Actor) RETURN a.name LIMIT 10");
-			return result.list(record -> record.get("a.name").asString());
+			StatementResult result = session.run("MATCH (a:Actor) OPTIONAL MATCH (a)<-[:ACTED_IN]-(m:Movie) RETURN a.name AS name, a.actorId AS actorId, collect(m.name) AS movies LIMIT 10");
+			return result.list(record -> {
+				String name = record.get("name").asString();
+				String actorId = record.get("actorId").asString();
+				List<String> movies = record.get("movies").asList(Value::asString);
+				return "<tr><td>" + name + "</td><td>" + actorId + "</td><td>" + (movies.isEmpty() ? "N/A" : String.join(", ", movies)) + "</td></tr>";
+			});
 		}
 	}
 
-	/**
-	 * Fetches the first 10 movies from the Neo4j database.
-	 * <p>
-	 * For each movie, it retrieves the name, movieId, and the list of actors that have acted in it.
-	 * </p>
-	 *
-	 * @return A list of HTML table rows with details about the movies.
-	 */
 	private List<String> fetchFirstTenMovies() {
-		// Implement database query to fetch first 10 movies
 		try (Session session = driver.session()) {
-			StatementResult result = session.run("MATCH (m:Movie) RETURN m.name LIMIT 10");
-			return result.list(record -> record.get("m.name").asString());
+			StatementResult result = session.run("MATCH (m:Movie) OPTIONAL MATCH (m)<-[:ACTED_IN]-(a:Actor) RETURN m.name AS title, m.movieId AS movieId, collect(a.name) AS actors LIMIT 10");
+			return result.list(record -> {
+				String title = record.get("title").asString();
+				String movieId = record.get("movieId").asString();
+				List<String> actors = record.get("actors").asList(Value::asString);
+				return "<tr><td>" + title + "</td><td>" + movieId + "</td><td>" + (actors.isEmpty() ? "N/A" : String.join(", ", actors)) + "</td></tr>";
+			});
 		}
 	}
 
