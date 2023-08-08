e13411060d7a7dd808b0a6cf6c1246bee7349fc7
Tyluur <contact@tyluur.com>
2023-08-07 21:34:51 -0400

    fix: add javadoc and logging to AddMovieHandler

--- src/main/java/ca/yorku/eecs/api/put/AddMovieHandler.java
+++ src/main/java/ca/yorku/eecs/api/put/AddMovieHandler.java
@@ -6,6 +6,8 @@ import com.sun.net.httpserver.HttpExchange;
 import org.neo4j.driver.v1.*;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.json.JSONObject;
 
@@ -14,6 +16,9 @@ import org.json.JSONObject;
  * <p>
  * This handler checks if a movie with the given movieId already exists in the database.
  * If it does, a 400 status code is returned. If not, the movie is added and a 200 status code is returned.
+ * </p>
+ * <p>
+ * Implements the HttpHandler interface to handle the HTTP request and response.
  * </p>
  *
  * @since 2023-08-06
@@ -24,6 +29,11 @@ public class AddMovieHandler implements HttpHandler {
 	 * The Neo4j database driver instance used for database operations.
 	 */
 	private final Driver driver;
+
+	/**
+	 * Logger for this class.
+	 */
+	private static final Logger logger = Logger.getLogger(AddMovieHandler.class.getName());
 
 	/**
 	 * Constructs a new AddMovieHandler with the provided Neo4j driver.
@@ -46,6 +56,8 @@ public class AddMovieHandler implements HttpHandler {
 	 */
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
+		logger.info("Received request to add movie");
+
 		String body = Utils.getBody(exchange);
 
 		try (Session session = driver.session()) {
@@ -57,12 +69,13 @@ public class AddMovieHandler implements HttpHandler {
 				StatementResult result = tx.run("MATCH (m:Movie {movieId: $movieId}) RETURN m", Values.parameters("movieId", movieId));
 
 				if (result.hasNext()) {
+					logger.warning("Attempted to add movie with existing movieId");
 					String response = "Movie with given movieId already exists.";
 					exchange.sendResponseHeaders(400, response.length());
 					exchange.getResponseBody().write(response.getBytes());
 				} else {
 					tx.run("CREATE (m:Movie {name: $name, movieId: $movieId})", Values.parameters("name", name, "movieId", movieId));
-
+					logger.info("Movie added successfully");
 					String response = "Movie added successfully.";
 					exchange.sendResponseHeaders(200, response.length());
 					exchange.getResponseBody().write(response.getBytes());
@@ -71,6 +84,7 @@ public class AddMovieHandler implements HttpHandler {
 				tx.success();
 			}
 		} catch (Exception e) {
+			logger.log(Level.SEVERE, "Internal server error: " + e.getMessage(), e);
 			String response = "Internal server error.";
 			exchange.sendResponseHeaders(500, response.length());
 			exchange.getResponseBody().write(response.getBytes());
