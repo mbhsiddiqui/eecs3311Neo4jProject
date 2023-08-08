71813f4e6d6f17b6b15b329da03fa7ea39376750
Tyluur <contact@tyluur.com>
2023-08-07 21:20:35 -0400

    chore: add javadoc to GetMovieHandlerTest

--- src/test/java/ca/yorku/eecs/api/get/GetMovieHandlerTest.java
+++ src/test/java/ca/yorku/eecs/api/get/GetMovieHandlerTest.java
@@ -14,30 +14,58 @@ import java.net.URI;
 
 import static org.mockito.Mockito.*;
 
+/**
+ * This class is responsible for testing the GetMovieHandler.
+ * It checks for different scenarios using Mockito to mock dependencies.
+ *
+ * @since 2023-08-07
+ */
 @RunWith(MockitoJUnitRunner.class)
 public class GetMovieHandlerTest {
 
+	/**
+	 * Mock of the HttpExchange class. This is the argument that will be passed to the handle method
+	 * of GetMovieHandler.
+	 */
 	@Mock
 	private HttpExchange httpExchange;
 
+	/**
+	 * Mock of the Driver class, which is the Neo4j database driver.
+	 * This is the argument that will be passed to the constructor of GetMovieHandler.
+	 */
 	@Mock
 	private Driver driver;
 
+	/**
+	 * Mock of the Session class. This is used to mock the database session.
+	 */
 	@Mock
 	private Session session;
 
-	@Mock
-	private Transaction transaction;
-
+	/**
+	 * Mock of the StatementResult class. This is used to mock the result of the database query.
+	 */
 	@Mock
 	private StatementResult statementResult;
 
+	/**
+	 * Mock of the OutputStream class. This is used to mock the output stream of the HttpExchange.
+	 */
 	@Mock
 	private OutputStream outputStream;
 
+	/**
+	 * Mock of the Record class. This is used to mock a record of the database query result.
+	 */
 	@Mock
 	private Record record;
 
+	/**
+	 * This method is called before each test. It sets up the mocks.
+	 *
+	 * @throws IOException If there's an issue with input or output.
+	 */
 	@Before
 	public void setUp() throws IOException {
 		when(httpExchange.getRequestURI()).thenReturn(URI.create("/api/v1/getMovie?movieId=123"));
@@ -46,6 +74,11 @@ public class GetMovieHandlerTest {
 		when(session.run(anyString(), any(Value.class))).thenReturn(statementResult);
 	}
 
+	/**
+	 * This test verifies the successful retrieval of a movie.
+	 *
+	 * @throws IOException If there's an issue with input or output.
+	 */
 	@Test
 	public void testGetMovieHandlerSuccess() throws IOException {
 		when(statementResult.hasNext()).thenReturn(true);
@@ -58,6 +91,11 @@ public class GetMovieHandlerTest {
 		verify(outputStream).close();
 	}
 
+	/**
+	 * This test verifies the case where a movie is not found.
+	 *
+	 * @throws IOException If there's an issue with input or output.
+	 */
 	@Test
 	public void testGetMovieHandlerMovieNotFound() throws IOException {
 		when(statementResult.hasNext()).thenReturn(false);
@@ -69,6 +107,11 @@ public class GetMovieHandlerTest {
 		verify(outputStream).close();
 	}
 
+	/**
+	 * This test verifies the case where movieId is not provided in the URL.
+	 *
+	 * @throws IOException If there's an issue with input or output.
+	 */
 	@Test
 	public void testGetMovieHandlerNoMovieId() throws IOException {
 		when(httpExchange.getRequestURI()).thenReturn(URI.create("/api/v1/getMovie"));
