5b7c2f1550f0a3a59e752729c241450fd9c1b59e
Tyluur <contact@tyluur.com>
2023-08-07 21:17:08 -0400

    chore: add javadoc to ComputeBaconNumberHandlerTest

--- src/test/java/ca/yorku/eecs/api/get/ComputeBaconNumberHandlerTest.java
+++ src/test/java/ca/yorku/eecs/api/get/ComputeBaconNumberHandlerTest.java
@@ -14,30 +14,64 @@ import java.net.URI;
 
 import static org.mockito.Mockito.*;
 
+/**
+ * This class is responsible for testing the ComputeBaconNumberHandler.
+ * It checks for different scenarios using Mockito to mock dependencies.
+ *
+ * @since 2023-08-07
+ */
 @RunWith(MockitoJUnitRunner.class)
 public class ComputeBaconNumberHandlerTest {
 
+	/**
+	 * Mock of the HttpExchange class. This is the argument that will be passed to the handle method
+	 * of ComputeBaconNumberHandler.
+	 */
 	@Mock
 	private HttpExchange httpExchange;
 
+	/**
+	 * Mock of the Driver class, which is the Neo4j database driver.
+	 * This is the argument that will be passed to the constructor of ComputeBaconNumberHandler.
+	 */
 	@Mock
 	private Driver driver;
 
+	/**
+	 * Mock of the Session class. This is used to mock the database session.
+	 */
 	@Mock
 	private Session session;
 
+	/**
+	 * Mock of the StatementResult class. This is used to mock the result of the database query.
+	 */
 	@Mock
 	private StatementResult statementResult;
 
+	/**
+	 * Mock of the OutputStream class. This is used to mock the output stream of the HttpExchange.
+	 */
 	@Mock
 	private OutputStream outputStream;
 
+	/**
+	 * Mock of the Record class. This is used to mock a record of the database query result.
+	 */
 	@Mock
 	private Record record;
 
+	/**
+	 * Mock of the Value class. This is used to mock the value of a field in the database record.
+	 */
 	@Mock
 	private Value value;
 
+	/**
+	 * This method is called before each test. It sets up the mocks.
+	 *
+	 * @throws IOException If there's an issue with input or output.
+	 */
 	@Before
 	public void setUp() throws IOException {
 		when(httpExchange.getResponseBody()).thenReturn(outputStream);
@@ -45,6 +79,11 @@ public class ComputeBaconNumberHandlerTest {
 		when(session.run(anyString(), any(Value.class))).thenReturn(statementResult);
 	}
 
+	/**
+	 * This test verifies the successful computation of the Bacon number.
+	 *
+	 * @throws IOException If there's an issue with input or output.
+	 */
 	@Test
 	public void testComputeBaconNumberHandlerSuccess() throws IOException {
 		when(httpExchange.getRequestURI()).thenReturn(URI.create("/api/v1/computeBaconNumber?actorId=123"));
@@ -60,6 +99,11 @@ public class ComputeBaconNumberHandlerTest {
 		verify(outputStream).close();
 	}
 
+	/**
+	 * This test verifies the case where no path to Kevin Bacon is found.
+	 *
+	 * @throws IOException If there's an issue with input or output.
+	 */
 	@Test
 	public void testComputeBaconNumberHandlerNoPathFound() throws IOException {
 		when(httpExchange.getRequestURI()).thenReturn(URI.create("/api/v1/computeBaconNumber?actorId=123"));
@@ -72,6 +116,11 @@ public class ComputeBaconNumberHandlerTest {
 		verify(outputStream).close();
 	}
 
+	/**
+	 * This test verifies the case where actorId is not provided in the URL.
+	 *
+	 * @throws IOException If there's an issue with input or output.
+	 */
 	@Test
 	public void testComputeBaconNumberHandlerNoActorId() throws IOException {
 		when(httpExchange.getRequestURI()).thenReturn(URI.create("/api/v1/computeBaconNumber"));
