c2d23a61c8ced108df79ef7856a9a5eb50019865
Tyluur <contact@tyluur.com>
2023-08-07 21:28:43 -0400

    fix: add exception handling to App

--- src/main/java/ca/yorku/eecs/App.java
+++ src/main/java/ca/yorku/eecs/App.java
@@ -3,6 +3,8 @@ package ca.yorku.eecs;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import ca.yorku.eecs.api.get.*;
 import ca.yorku.eecs.api.put.AddActorHandler;
@@ -14,51 +16,43 @@ import org.neo4j.driver.v1.Config;
 import org.neo4j.driver.v1.Driver;
 import org.neo4j.driver.v1.GraphDatabase;
 
-/**
- * The App class sets up and runs the HTTP server.
- * It sets up the routes for the different API endpoints and starts the server.
- *
- * @since 2023-08-07
- */
 public class App {
 
-	/**
-	 * The port on which the server will listen.
-	 */
-	static int PORT = 8080;
+	// Server port made constant
+	private static final int PORT = 8080;
 
-	/**
-	 * The main method sets up and starts the HTTP server.
-	 *
-	 * @param args the command line arguments. This is not used.
-	 * @throws IOException if there is an error setting up the server.
-	 */
-	public static void main(String[] args) throws IOException {
-		// Creating the server
-		HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", PORT), 0);
+	// Logger for this class
+	private static final Logger logger = Logger.getLogger(App.class.getName());
 
-		// Setting up the database driver
-		Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "12345678"), Config.build().withoutEncryption().toConfig());
+	public static void main(String[] args) {
+		try {
+			// Create the HTTP server
+			HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", PORT), 0);
 
-		// Setting up the PUT endpoints
-		server.createContext("/api/v1/addActor", new AddActorHandler(driver));
-		server.createContext("/api/v1/addMovie", new AddMovieHandler(driver));
-		server.createContext("/api/v1/addRelationship", new AddRelationshipHandler(driver));
+			// Initialize Neo4j driver
+			Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "12345678"), Config.build().withoutEncryption().toConfig());
 
-		// Setting up the GET endpoints
-		server.createContext("/api/v1/getActor", new GetActorHandler(driver));
-		server.createContext("/api/v1/getMovie", new GetMovieHandler(driver));
-		server.createContext("/api/v1/hasRelationship", new HasRelationshipHandler(driver));
-		server.createContext("/api/v1/computeBaconNumber", new ComputeBaconNumberHandler(driver));
-		server.createContext("/api/v1/computeBaconPath", new ComputeBaconPathHandler(driver));
+			// Create context for each API endpoint with corresponding handlers
+			server.createContext("/api/v1/addActor", new AddActorHandler(driver));
+			server.createContext("/api/v1/addMovie", new AddMovieHandler(driver));
+			server.createContext("/api/v1/addRelationship", new AddRelationshipHandler(driver));
+			server.createContext("/api/v1/getActor", new GetActorHandler(driver));
+			server.createContext("/api/v1/getMovie", new GetMovieHandler(driver));
+			server.createContext("/api/v1/hasRelationship", new HasRelationshipHandler(driver));
+			server.createContext("/api/v1/computeBaconNumber", new ComputeBaconNumberHandler(driver));
+			server.createContext("/api/v1/computeBaconPath", new ComputeBaconPathHandler(driver));
 
-		// Setting the server's executor
-		server.setExecutor(Executors.newCachedThreadPool());
+			// Use a thread pool executor for the server
+			server.setExecutor(Executors.newCachedThreadPool());
 
-		// Starting the server
-		server.start();
+			// Start the server
+			server.start();
 
-		// Printing a message to indicate that the server has started
-		System.out.printf("Server started on port %d...\n", PORT);
+			logger.info(String.format("Server started on port %d...\n", PORT));
+		} catch (IOException e) {
+			// Log the error and exit the application
+			logger.log(Level.SEVERE, "Error starting the server: " + e.getMessage(), e);
+			System.exit(1);
+		}
 	}
 }
