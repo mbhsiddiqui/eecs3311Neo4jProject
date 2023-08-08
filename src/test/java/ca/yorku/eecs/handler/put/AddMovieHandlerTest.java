c6c9eda7f22d133d9281ea5edb4cc0d5d6aca8bd
Tyluur <contact@tyluur.com>
2023-08-08 00:20:05 -0400

    fix: update documentation to AddMovieHandlerTest

--- src/test/java/ca/yorku/eecs/handler/put/AddMovieHandlerTest.java
+++ src/test/java/ca/yorku/eecs/handler/put/AddMovieHandlerTest.java
@@ -15,37 +15,54 @@ import java.io.OutputStream;
 import static org.mockito.Mockito.*;
 
 /**
- * This class contains unit tests for the AddMovieHandler class.
- * It tests both the success and failure scenarios when a movie is added.
+ * Unit test class for the AddMovieHandler class. It includes tests for both
+ * success and failure scenarios when attempting to add a movie.
  *
  * @since 2023-08-06
  */
 @RunWith(MockitoJUnitRunner.class)
 public class AddMovieHandlerTest {
 
+	/**
+	 * Mock object to simulate HTTP exchange.
+	 */
 	@Mock
 	private HttpExchange httpExchange;
 
+	/**
+	 * Mock object to simulate the Neo4j database driver.
+	 */
 	@Mock
 	private Driver driver;
 
+	/**
+	 * Mock object to simulate a database session.
+	 */
 	@Mock
 	private Session session;
 
+	/**
+	 * Mock object to simulate a database transaction.
+	 */
 	@Mock
 	private Transaction transaction;
 
+	/**
+	 * Mock object to simulate the result of a database statement.
+	 */
 	@Mock
 	private StatementResult statementResult;
 
+	/**
+	 * Mock object to simulate an output stream for HTTP response.
+	 */
 	@Mock
 	private OutputStream outputStream;
 
 	/**
-	 * This method sets up the common mock behaviors used in the test methods.
-	 * It is run before each test case.
+	 * Sets up the common mock behaviors for all tests. This method is run before each test.
 	 *
-	 * @throws IOException If an I/O error occurs while setting up the mocks.
+	 * @throws IOException If there is an error setting up the input or output streams.
 	 */
 	@Before
 	public void setUp() throws IOException {
@@ -57,10 +74,10 @@ public class AddMovieHandlerTest {
 	}
 
 	/**
-	 * This test case tests the scenario where a new movie is successfully added.
-	 * In this scenario, the movie does not exist in the database before the operation.
+	 * Tests the successful addition of a new movie. This test case assumes the movie does not
+	 * exist in the database prior to the operation.
 	 *
-	 * @throws IOException If there is an error reading the HTTP request body.
+	 * @throws IOException If there is an error writing to the output stream.
 	 */
 	@Test
 	public void testAddMovieHandlerSuccess() throws IOException {
@@ -75,9 +92,9 @@ public class AddMovieHandlerTest {
 	}
 
 	/**
-	 * This test case tests the scenario where the addition of a movie fails because the movie already exists.
+	 * Tests the failure of adding a new movie due to the movie already existing in the database.
 	 *
-	 * @throws IOException If there is an error reading the HTTP request body.
+	 * @throws IOException If there is an error writing to the output stream.
 	 */
 	@Test
 	public void testAddMovieHandlerFailure() throws IOException {
